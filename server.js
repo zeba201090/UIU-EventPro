b2689a5387d1b2538daa3aa7e887d00454c30d5d
zeba201090 <mzeba16@gmail.com>
2023-06-19 00:03:44 +0600

    booking slot done

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { start } = require("./db_conn");
-const session = require('express-session');
-const path = require('path');
+const session = require("express-session");
+const path = require("path");
 const ejs = require("ejs");
-const mime = require('mime');
+const mime = require("mime");
 const app = express();
 start();
-
-
-
 
 const User = require("./models/model_schemaa");
 const Room = require("./models/room_schema");
 dotenv.config();
-app.use('/select.css', express.static(path.join(__dirname, 'Public/select.css')));
+app.use(
+  "/select.css",
+  express.static(path.join(__dirname, "Public/select.css"))
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -23,16 +23,15 @@ app.set("view engine", "ejs");
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
 });
-app.use(session({
-  secret: '1234', 
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  session({
+    secret: "1234",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-const createRoomm = require('./router/room');
-
-
-
+const createRoomm = require("./router/room");
 
 // Connect to DB
 app.get("/", (req, res) => {
@@ -43,7 +42,7 @@ function requireLogin(req, res, next) {
   if (req.session.loggedIn) {
     next(); // User is authenticated, proceed to the next middleware/route handler
   } else {
-    res.redirect('/'); // User is not logged in, redirect to the login page
+    res.redirect("/"); // User is not logged in, redirect to the login page
   }
 }
 
@@ -73,31 +72,27 @@ app.post("/submit", async (req, res) => {
 app.post("/login", async (req, res) => {
   const pass = req.body.loginpassword;
   const email_ = req.body.username;
- 
+
   const email_db = await User.findOne({ email: email_ });
   const pass_db = email_db.password;
-  
+
   if (pass === pass_db) {
     req.session.loggedIn = true;
     req.session.userId = User._id;
     // res.send("Successfully logged in");
     res.render("homepage", { name: email_db.firstName });
-   
   } else {
     res.send("password not matching");
   }
 });
 
-app.post("/rooms" , async (req, res) => {
-  const { roomName, roomNumber, roomType, capacity} = req.body;
+app.post("/rooms", async (req, res) => {
+  const { roomName, roomNumber, roomType, capacity } = req.body;
   let days = [];
-  
-  days = req.body.checkbox || [];
-  const availableTime =  req.body.myArray.split(',') || [];
-  console.log(availableTime);
 
- 
-  
+  days = req.body.checkbox || [];
+  const availableTime = req.body.myArray.split(",") || [];
+  console.log(availableTime);
 
   const room = new Room({
     roomName: roomName,
@@ -114,56 +109,63 @@ app.post("/rooms" , async (req, res) => {
   res.send(savedRoom);
 });
 
-app.post("/avl", async(req,res) => {
+app.post("/avl", async (req, res) => {
   const days = req.body.checkbox;
   console.log(days);
   res.send(days);
-})
+});
 
-app.get("/book",requireLogin, async (req, res) => { 
-  res.render("book")
-;});
+app.get("/book",  async (req, res) => {
+  res.render("book");
+});
 
-
-app.post('/bookroom',requireLogin , async(req, res) => {
+app.post("/bookroom",  async (req, res) => {
   const selectedRoomType = req.body.roomType; // Retrieve the selected room type from the form
-   let data =[];
 
-      data = await Room.find({ roomType: selectedRoomType });
-      const rooms= data.length;
-      res.render('rooms', { rooms: data,roomLength:rooms ,roomType:selectedRoomType, availableTime: data.availableTime });
+  let data = [];
 
-    
+  data = await Room.find({ roomType: selectedRoomType });
+  const rooms = data.length;
+  res.render("rooms", {
+    rooms: data,
+    roomLength: rooms,
+    roomType: selectedRoomType,
+    availableTime: data.availableTime,
+    bookedSlots: data.bookedSlots,
+  });
 });
-app.post('/booking', async(req, res) => {
+app.post("/booking", async (req, res) => {
   const roomId = req.body.selectedRoom;
-  console.log("Selected Room Number:", roomId);
-  // Room.update({ roomNumber: selectedRoom }, { $set: { booking: true } });
-  const updatedRoom = await Room.findOneAndUpdate(
-    { _id: roomId },
-    { $set: { booking: true }})
+  let unavailableT = [];
+  unavailableT = req.body.bookedSlots;
 
-  res.send("Room Booked Successfully");
+  console.log("Unavailable Time:", unavailableT);
+
+  const arraySlots = Object.values(unavailableT);
+  const updatedRoom = await Room.updateOne({ _id: roomId },   { $push: { bookedSlots: arraySlots  } })
+
+  
+ 
+  res.send(updatedRoom);
 });
 
-app.post('/slot',async(req,res) => {
-  const receivedArray = req.body.myArray.split(',');
-  console.log('Received array:', receivedArray);
+
+app.post("/slot", async (req, res) => {
+  const receivedArray = req.body.myArray.split(",");
+  console.log("Received array:", receivedArray);
 
   // Handle the array data as needed
   // ...
 
-  res.send('Array received successfully');
+  res.send("Array received successfully");
 });
 
-app.get('/logout', (req, res) => {
+app.get("/logout", (req, res) => {
   // Clear the session data
   req.session.destroy((err) => {
     if (err) {
-      console.error('Error destroying session:', err);
+      console.error("Error destroying session:", err);
     }
-    res.redirect('/'); // Redirect to the login page after logout
+    res.redirect("/"); // Redirect to the login page after logout
   });
 });
-
-
